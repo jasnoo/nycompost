661ed304b8db4d59618d43715eb3a62b42e4ebb3
Jasmine <jasminee@gmail.com>
2022-07-20 16:04:20 -0400

    updated server

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server started at port 3000`)
+})
