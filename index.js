a1d4a295daa79b8030b7b73f393fd08c56bb9b3c
Jasmine <jasminee@gmail.com>
2022-07-24 20:12:29 -0400

    updated mongo config

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+const DB_URI = process.env.MONGO_URI
+
+mongoose.connect(DB_URI);
+const db = mongoose.connection
+
+db.on('error', (error) => {
+    console.log(error)
+})
+
+db.once('connected', () => {
+    console.log('DB Connected');
+})
 
 const app = express();
 app.use(express.json());
@@ -7,3 +20,4 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server started at port 3000`)
 })
+
