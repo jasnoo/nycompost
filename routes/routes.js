9ddfebe23d9b8ecb851173a36921c28e533e45d9
Jasmine <jasminee@gmail.com>
2022-08-02 14:20:20 -0400

    added route to get all sites

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router()
+const Model = require('../models/siteModel')
 
+// Get all site 
+router.get('/all', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 module.exports = router;
