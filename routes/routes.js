05e8a2a3ac3bfdb613d6708fc14e4b5d65812079
Jasmine <jasminee@gmail.com>
2022-08-02 14:24:47 -0400

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Model = require('../models/siteModel')
 
 // Get all site 
-router.get('/all', async (req, res) => {
+router.get('/find', async (req, res) => {
     try {
         const data = await Model.find();
         res.json(data)
@@ -12,5 +12,17 @@ router.get('/all', async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 })
+
+router.get('/find/:borough', async (req, res) => {
+    try {
+        const data = await Model.find({ borough: req.param.borough }).exec();
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
 
 module.exports = router;
