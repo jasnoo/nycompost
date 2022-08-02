e69225686a117307cfd6a385e6b3f9ea664f0479
Jasmine <jasminee@gmail.com>
2022-08-02 14:45:57 -0400

    added schedule controller to help determine chosen times

---
+++ controllers/scheduleController.js
@@ -0,0 +1,10 @@
+const scheduleController = {}
+
+scheduleController.confirmTime = {
+    // based on user's current date/time, should pass along relevant details to determine available places
+
+    // may want to split this up to reflect either choosing available dates or current date
+}
+
+
+module.exports = schedule
---
+++ controllers/siteController.js
@@ -0,0 +1,26 @@
+const Site = require('../models/siteModel');
+
+const siteController = {}
+
+siteController.add = {
+    // add new user-submited site 
+}
+
+siteController.delete = {
+    // remove user-added site
+}
+
+siteController.update = {
+    // update user-added site
+}
+
+siteController.getAll = {
+    // get all current sites 
+}
+
+siteController.get = {
+    // should provide functionality to filter by Borough, currently open (month/day), can dispose meat,
+}
+
+
+module.exports = siteController
